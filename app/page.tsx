388b8975e51a4cd4bebc6086c07ef58e492d2bc8
yadavharsh2004 <harshy8013698@gmail.com>
2025-06-06 23:53:05 +0530

    Setup the layout for HomePage in main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,46 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline text-center mt-20'> Welcome to my Saas App</h1>
-      <Button className='cursor-pointer mt-20 flex mx-auto'>
-        Let's get started
-      </Button>
-    </div>
+    <main>
+      <h1> Popular Companions</h1>
+
+      <section className='home-section'>
+        <CompanionCard 
+          id = "123"
+          name = "Neura the Brainy Explorer"
+          topic = "Neural Network of the Brain"
+          duration = [45]
+          color = "#ffda6e"
+
+        />
+        <CompanionCard 
+          id = "456"
+          name = "Neura the Brainy Explorer"
+          topic = "Neural Network of the Brain"
+          duration = [45]
+          color = "#ffda6e"
+
+        />
+        <CompanionCard 
+          id = "789"
+          name = "Neura the Brainy Explorer"
+          topic = "Neural Network of the Brain"
+          duration = [45]
+          color = "#ffda6e"
+
+        />
+      </section>
+
+      <section className='home-section'>
+        <CompanionsList />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
