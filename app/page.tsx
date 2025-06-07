18455992abbc3b6891f03551d25f353fe98a51d8
yadavharsh2004 <harshy8013698@gmail.com>
2025-06-07 13:01:29 +0530

    Implement Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import CompanionCard from '@/components/CompanionCard'
 import CompanionsList from '@/components/CompanionsList'
 import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
@@ -14,30 +15,34 @@ const Page = () => {
           id = "123"
           name = "Neura the Brainy Explorer"
           topic = "Neural Network of the Brain"
-          duration = [45]
+          subject= "Science"
+          duration = {45}
           color = "#ffda6e"
-
-        />
+          />
         <CompanionCard 
           id = "456"
-          name = "Neura the Brainy Explorer"
-          topic = "Neural Network of the Brain"
-          duration = [45]
-          color = "#ffda6e"
-
-        />
+          name = "Countsy the Number Wizard"
+          topic = "Derivatives and Integrals"
+          subject= "Maths"
+          duration = {30}
+          color = "#e5d0ff"
+          />
         <CompanionCard 
           id = "789"
-          name = "Neura the Brainy Explorer"
-          topic = "Neural Network of the Brain"
-          duration = [45]
-          color = "#ffda6e"
-
+          name = "Verba the Vocabulary Builder"
+          topic = "language"
+          subject= "English"
+          duration = {30}
+          color = "#bde7ff"
         />
       </section>
 
       <section className='home-section'>
-        <CompanionsList />
+        <CompanionsList 
+          title = "Recently Completed Sessions"
+          companions = {recentSessions}
+          classNames = "w-2/3 max-lg:w-full "
+        />
         <CTA />
       </section>
     </main>
