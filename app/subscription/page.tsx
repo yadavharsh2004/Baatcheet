5743a6c37445fa7fbcc8b75fe31567fc66abf5f0
yadavharsh2004 <harshy8013698@gmail.com>
2025-06-08 16:21:04 +0530

    Add Clerk billing with PricingTable

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,8 +1,11 @@
+import { PricingTable } from '@clerk/nextjs'
 import React from 'react'
 
 const Subscription = () => {
   return (
-    <div>Subscription</div>
+    <div>
+      <PricingTable />
+    </div>
   )
 }
 
