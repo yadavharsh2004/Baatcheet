950e4c5a79ea90a9bf5a8300591eefbe82135c0c
yadavharsh2004 <harshy8013698@gmail.com>
2025-06-12 00:25:54 +0530

    Add Get companion by id function and implement header of the companion session

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,8 +1,58 @@
+import { getCompanion } from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 
+interface CompanionSessionPageProps {
+  params : Promise<{id: string}>;
+}
 
-const CompanionSession = () => {
+//params  /url/{id}
+// searchParams  /url/?key=value&key1=value1 
+
+const CompanionSession = async ({params} : CompanionSessionPageProps) => {
+  const {id} = await params;
+  const {name, title, subject, topic, duration, } = await getCompanion(id);
+  const user = await currentUser();
+
+  if(!user) redirect('/sign-in');
+  if(!name) redirect('/companions');
+
   return (
-    <div>CompanionSession</div>
+    <main>
+      <article className="flex rounded-border justify-between p-6 max-md:flex-col ">
+        <div className="flex items-center gap-2 ">
+
+          <div className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden "
+            style={{backgroundColor: getSubjectColor(subject)}}
+          >
+            <Image src={`/icons/${subject}.svg`} alt="subject" height={35} width={35} />
+          </div>
+
+          <div className="flex flex-col gap-2  ">
+            <div className="flex items-center gap-2">
+
+              <p className="font-bold text-2xl">
+                {name}
+              </p>
+
+              <div className="subject-badge max-sm:hidden">
+                {subject}
+              </div>
+
+            </div>
+            <p className="text-lg">{topic}</p>
+          </div>
+
+        </div>
+
+        <div className="items-start text-2xl max-md:hidden">
+          {duration} minutes
+        </div>
+      </article>
+
+    </main>
   )
 }
 
